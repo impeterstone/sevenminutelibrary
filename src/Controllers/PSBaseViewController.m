c79e7c4d69c1a4d1657d040d6aba88aef2feb994
Peter Shih <ptshih@gmail.com>
2011-09-29 13:04:54 -0700

    remove reload on resume

--- src/Controllers/PSBaseViewController.m
+++ src/Controllers/PSBaseViewController.m
@@ -90,12 +90,12 @@
 
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reloadDataSource) name:kApplicationResumed object:nil];
+//  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reloadDataSource) name:kApplicationResumed object:nil];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
   [super viewWillDisappear:animated];
-  [[NSNotificationCenter defaultCenter] removeObserver:self name:kApplicationResumed object:nil];
+//  [[NSNotificationCenter defaultCenter] removeObserver:self name:kApplicationResumed object:nil];
 }
 
 - (void)orientationChangedFromNotification:(NSNotification *)notification {
